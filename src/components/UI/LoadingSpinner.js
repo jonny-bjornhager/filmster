238d9b669007fa9aaecc8741d0a19b062ca37cb0
Jonny Björnhager <jonny.bjornhager@live.se>
2022-03-13 18:50:39 +0100

    Add loading spinner to home page

---
+++ src/components/UI/LoadingSpinner.js
@@ -0,0 +1,7 @@
+import classes from "./LoadingSpinner.module.css";
+
+const LoadingScreen = () => {
+  return <div className={classes["loading-spinner"]}></div>;
+};
+
+export default LoadingScreen;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,27 @@
 import classes from "./Home.module.css";
 import Header from "../Layout/Header";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
 import CardSwiper from "../UI/CardSwiper";
 import { popularMoviesUrl, popularTvShowsUrl } from "../../api_urls";
 import { useFetchPopularMedia } from "../../hooks/useFetchPopularMedia";
 
 const Home = () => {
-  const { media } = useFetchPopularMedia(popularMoviesUrl);
+  const { media, isLoading } = useFetchPopularMedia(popularMoviesUrl);
 
   return (
     <>
-      <Header movies={media.slice(0, 6)} />
-      <section className={classes.popular}>
-        <CardSwiper type="movie" fetchUrl={popularMoviesUrl} />
-        <CardSwiper type="tv" fetchUrl={popularTvShowsUrl} />
-      </section>
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && (
+        <>
+          {" "}
+          <Header movies={media.slice(0, 6)} />
+          <section className={classes.popular}>
+            <CardSwiper type="movie" fetchUrl={popularMoviesUrl} />
+            <CardSwiper type="tv" fetchUrl={popularTvShowsUrl} />
+          </section>
+        </>
+      )}
     </>
   );
 };
