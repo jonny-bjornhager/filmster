36a020577c1c33a5c7e26e8a7dc1be0a291ad804
Jonny Björnhager <jonny.bjornhager@live.se>
2022-03-13 17:29:27 +0100

    Install react lazy load image component for lazy loading images

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,7 @@
+import classes from "./Header.module.css";
+
+const Header = ({ movies }) => {
+  return <header className={classes.header}></header>;
+};
+
+export default Header;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
 import classes from "./Home.module.css";
+import Header from "../Layout/Header";
+
 import { popularMoviesUrl } from "../../api_urls";
 import { useFetchPopularMedia } from "../../hooks/useFetchPopularMedia";
 
 const Home = () => {
   const { media } = useFetchPopularMedia(popularMoviesUrl);
-  console.log(media);
 
   return (
     <>
+      <Header movies={media} />
       <section className={classes.popular}></section>
     </>
   );
