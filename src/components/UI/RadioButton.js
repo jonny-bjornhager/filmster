588961bb1841226650dc3ca3ded90cd4cc1df8ec
Jonny Björnhager <jonny.bjornhager@live.se>
2022-03-19 10:57:48 +0100

    Add styling to radio buttons

--- src/components/UI/RadioButton.js
+++ src/components/UI/RadioButton.js
@@ -1,11 +1,19 @@
 import classes from "./RadioButton.module.css";
 
-const RadioButton = () => {
+const RadioButton = ({ choice, children, name }) => {
   return (
-    <div className={classes["radio-control"]}>
-      <label htmlFor="search-type-choice"></label>
-      <input id="search-type-choice" type="radio" />
-    </div>
+    <>
+      <input
+        className={classes["radio-input"]}
+        name={name}
+        id={choice}
+        type="radio"
+        value={choice}
+      />
+      <label className={classes["radio-label"]} htmlFor={choice}>
+        {children}
+      </label>
+    </>
   );
 };
 
--- src/components/UI/SearchForm.js
+++ src/components/UI/SearchForm.js
@@ -2,6 +2,7 @@ import classes from "./SearchForm.module.css";
 
 import Button from "./Button";
 import SearchBar from "./SearchBar";
+import RadioButton from "./RadioButton";
 
 const SearchForm = ({ changeHandler, onSubmit, searchValue }) => {
   return (
@@ -11,6 +12,14 @@ const SearchForm = ({ changeHandler, onSubmit, searchValue }) => {
         <Button type="submit" variant="red">
           Search
         </Button>
+      </div>
+      <div className={classes["radio-btns"]}>
+        <RadioButton name="type-choice" choice="movie">
+          Movies
+        </RadioButton>
+        <RadioButton name="type-choice" choice="tv">
+          Tv-shows
+        </RadioButton>
       </div>
     </form>
   );
